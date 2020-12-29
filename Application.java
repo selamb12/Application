286e5837569be90c9ae01920f69a5472efe000d8
peace <cffbob@gmail.com>
2020-12-29 18:41:48 -0500

    First Java commit

---
+++ Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber;
+		myNumber = 80;
+		short myshort = 847;
+		long myLong = 9797;
+		double myDouble = 76746;
+		float myFloat = 324.3f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+				
+		System.out.println(myNumber);
+		System.out.println(myshort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+}
